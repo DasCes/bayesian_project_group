3d0f58c9bd0d7d83b6e5fce420609f998dc0bca5
Gabbag2 <gabriel.rochon97410@gmail.com>
2025-11-13 18:30:33 +0100

    try 2

--- bayesian.py
+++ bayesian.py
@@ -1,38 +1,52 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import numpy as np
 
 
 def psycometrique(data_tuple):
     result, mean_used = data_tuple
-    array_mean_result = []
-    array_mean_used = []
+    true_array_mean_used = []
+    true_array_result = []
     n = len(result)
     block_size = 50
+    big_blocs = 11
+    size_one_block = block_size * big_blocs
+    
+    val = list(range(0, n, block_size))
+    
+    for j in range(4):
+        array_mean_result = []
+        array_mean_used = []
+        for i in range(0, size_one_block, block_size):
+            # bloc de result
+            block_r = result[size_one_block*j + i : size_one_block*j + i+block_size]
+            mean_r = sum(block_r) / len(block_r)
+            array_mean_result.append(mean_r)
 
-    for i in range(0, n, block_size):
-        # bloc de result
-        block_r = result[i:i+block_size]
-        mean_r = sum(block_r) / len(block_r)
-        array_mean_result.append(mean_r)
+            # bloc de mean_used
+            block_m = mean_used[size_one_block*j + i : size_one_block*j + i+block_size]
+            mean_m = sum(block_m) / len(block_m)
+            array_mean_used.append(mean_m)
 
-        # bloc de mean_used
-        block_m = mean_used[i:i+block_size]
-        mean_m = sum(block_m) / len(block_m)
-        array_mean_used.append(mean_m)
-
-    return array_mean_result, array_mean_used
+        true_array_result.append(array_mean_result)
+        true_array_mean_used.append(array_mean_used)
+    return true_array_result, true_array_mean_used
 
 
-def ploter(array_mean_s2, array_mean_decision):
-    plt.figure(figsize=(6,4))
-    plt.plot(array_mean_s2, array_mean_decision, 'o-', color='steelblue', label='Psychometric curve')
-    plt.xlabel('Mean S2 value')          # abscisse : stimulus
-    plt.ylabel('Mean decision (P[1])')   # ordonnée : proba de répondre 1
-    plt.title('Psychometric Function')
+def ploter(true_array_mean_used, true_array_result):
+    plt.figure(figsize=(7,5))
+
+    for idx, (mean_used, mean_res) in enumerate(zip(true_array_mean_used, true_array_result)):
+        plt.plot(mean_used, mean_res, 'o-', label=f'Bloc {idx+1}')
+
+    plt.xlabel('Mean S2 value')
+    plt.ylabel('Mean decision (P[1])')
+    plt.title('Psychometric Functions (all curves)')
     plt.grid(True, alpha=0.3)
     plt.legend()
+    plt.tight_layout()
     plt.show()
+
 
     
 def reading_csv(file_path):
@@ -40,6 +54,7 @@ def reading_csv(file_path):
     mean_used = df['S2_val'].tolist()
     result = df['Decision (S1>S2)'].tolist()
     data_tuple = (mean_used, result)
+    print(len(result))
     return data_tuple
 
 def main():
